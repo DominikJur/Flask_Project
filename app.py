558195d9c7b1f0ddf26e0d6afffb8c87b0b5610e
DominikJur <282214@student.pwr.edu.pl>
2024-07-03 17:16:51 +0200

    "WYKRESY DZIAŁAJĄ"

--- app.py
+++ app.py
@@ -1,39 +1,40 @@
-from flask import Flask, request, jsonify, render_template
 import numpy as np
+from flask import Flask, render_template, request, session
+
 from src.utils import plot_log
-import matplotlib.pyplot as plt
 
 app = Flask(__name__)
+app.secret_key = b"111_fx\1"
 
-@app.route('/')
+
+@app.route("/")
 def index():
     return render_template("index.html")
 
-@app.route('/index')
+
+@app.route("/index")
 def back_to_index():
     return render_template("index.html")
 
-@app.route('/authors')
+
+@app.route("/authors")
 def authors():
     return render_template("authors.html")
 
-@app.route('/zagadnienie', methods=['POST', 'GET'])
+
+@app.route("/zagadnienie", methods=["POST", "GET"])
 def zagadnienie():
-    base = np.e
-    xmin = 0.001
-    xmax = 10.001
-
-    if request.method=='POST':
+    print(session)
+    if request.method == "POST":
         try:
-            base = float(request.form['base'])
-            xmin = float(request.form["xmin"])
-            xmax = float(request.form["xmax"])
-        except Exception as e:
-            return render_template("zagadnienie.html", plot=plot_log(base=base, xmin=0.001, xmax=10.001))
-    plot = plot_log(base, xmin, xmax)
-        
-    return render_template("zagadnienie.html", plot=plot)
+            session["base"] = float(request.form["base"])
+        except ValueError:
+            session["base"] = np.e
+    base = session.get("base", np.e)
+    return render_template(
+        "zagadnienie.html", plot=plot_log(base=base, xmin=0.001, xmax=20.001)
+    )
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(debug=True)
